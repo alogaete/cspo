f9e1b4be5fbb9d0cceded2b7a9d4f1bedcd43b95
Alonso Gaete <agaete-sc@AloM3.local>
2025-07-11 11:39:45 -0400

    msg en vista

--- app/src/main/java/com/cs/po/mainActivity.kt
+++ app/src/main/java/com/cs/po/mainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,12 +16,19 @@ class MainActivity : AppCompatActivity() {
 
         val loginCodeInput = findViewById<EditText>(R.id.loginCodeInput)
         val button = findViewById<Button>(R.id.openIntentButton)
-
+        val msgText = findViewById<TextView>(R.id.msg)
 
         button.setOnClickListener {
             val loginCode = loginCodeInput.text.toString()
 
+
             val uriStr = "ccusafe://open?porteo_login_code=$loginCode"
+            msgText.text = "Sending: $uriStr"
+            // show a toast or log the message
+            Toast(this@MainActivity).apply {
+                setText("Sending: $uriStr")
+                show()
+            }
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uriStr))
             startActivity(intent)
         }
